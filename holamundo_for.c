ce3f2188b0175a7b163bfbee6fcef5f38dd72a4c
JonathanRA949 <riveracharly949@gmail.com>
2019-11-04 08:23:01 -0600

    Updeteando la 2da rama

---
+++ holamundo_for.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int i  = 0;
+char mensaje[10] = "Hola mundo";
+
+int main () {
+           for (i=1; i <= 10; i++) {
+		 printf ("%s %d \n", mensaje, i);
+                }
+		 return 0;
+}
+
+
+
+
+
